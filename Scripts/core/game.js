a4437bd943207ba26294a8663b7effdc3d405626
PsychoMo <zymike09@hotmail.com>
2018-02-22 15:03:05 -0500

    added button event listeners and handlers

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -12,6 +12,7 @@
     function Start() {
         console.log("Starting Application...");
         stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20); //turn this on for buttons
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update);
         Main();
@@ -19,6 +20,17 @@
     function Update() {
         // helloLabel.rotation -= 5;
         stage.update(); // redraws the stage
+    }
+    function clickMeButtonMouseOver() {
+        clickMeButton.alpha = 0.7;
+    }
+    function clickMeButtonMouseOut() {
+        clickMeButton.alpha = 1.0;
+    }
+    function clickMeButtonMouseClick() {
+        helloLabel.text = "Clicked!";
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     }
     function Main() {
         console.log("Game Started...");
@@ -30,6 +42,9 @@
         clickMeButton.x = 320;
         clickMeButton.y = 340;
         stage.addChild(clickMeButton);
+        clickMeButton.on("mouseover", clickMeButtonMouseOver);
+        clickMeButton.on("mouseout", clickMeButtonMouseOut);
+        clickMeButton.on("click", clickMeButtonMouseClick);
     }
     window.onload = Init;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -17,6 +17,7 @@
     console.log("Starting Application...")
 
     stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); //turn this on for buttons
     createjs.Ticker.framerate = 60; // 60 FPS
     createjs.Ticker.on("tick", Update);
     Main();
@@ -25,6 +26,21 @@
   function Update(): void {
     // helloLabel.rotation -= 5;
     stage.update(); // redraws the stage
+  }
+
+  function clickMeButtonMouseOver():void{
+    clickMeButton.alpha = 0.7;
+
+  }
+
+  function clickMeButtonMouseOut():void{
+    clickMeButton.alpha = 1.0;
+  }
+
+  function clickMeButtonMouseClick():void{
+    helloLabel.text = "Clicked!";
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
   }
 
   function Main(): void {
@@ -39,6 +55,10 @@
     clickMeButton.x = 320;
     clickMeButton.y = 340;
     stage.addChild(clickMeButton);
+
+    clickMeButton.on("mouseover", clickMeButtonMouseOver);
+    clickMeButton.on("mouseout", clickMeButtonMouseOut);
+    clickMeButton.on("click", clickMeButtonMouseClick);
   }
 
   window.onload = Init;
